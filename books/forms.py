f638b8b0d6bdd3d0069a61225560d9313e1abe8c
AllanSechrist <sechristallan@gmail.com>
2023-08-01 19:42:24 +0900

    add message to inform user to make a library before adding a book

--- books/forms.py
+++ books/forms.py
@@ -1,5 +1,11 @@
 from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import messages
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Fieldset, Submit
 from .models import Book
+from libraries.models import Library
 
 
 class BookForm(forms.ModelForm):
@@ -18,3 +24,17 @@ class BookForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         if user is not None:
             self.fields["library"].queryset = self.fields["library"].queryset.filter(user=user)
+        
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Fieldset(
+            "",
+            "title",
+            "author",
+            "publisher",
+            "isbn",
+            "level",
+            "library",
+            ),
+            Submit('submit', 'Add Book', css_class="btn btn-primary")
+        )
--- libraries/forms.py
+++ libraries/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Fieldset, Submit
 from .models import Library
 
 
@@ -8,3 +10,15 @@ class LibraryForm(forms.ModelForm):
         fields = [
             "label",
         ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Fieldset(
+            "",
+            "label",
+            ),
+            Submit('submit', "Add Library", css_class="btn btn-primary")
+        )
